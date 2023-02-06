5717b21554acba0f5f7f22c0f16c17b416d08b9b
AlonsoSMdV <115071431+AlonsoSMdV@users.noreply.github.com>
2023-02-06 18:51:38 +0100

    Commit

---
+++ Capitulo_01/Ejercicio02.java
@@ -0,0 +1,9 @@
+package Capitulo_01;
+
+public class Ejercicio02 {
+    public static void main(String args[]){
+        System.out.println("Alonso Sánchez Moreno de Vega");
+        System.out.println("Calle Eneas/58");
+        System.out.println("678 54 57 89");
+    }
+}
