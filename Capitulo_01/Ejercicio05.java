eea7c61fb8a892f02a4fe8cb7e3ba15afdec4635
AlonsoSMdV <115071431+AlonsoSMdV@users.noreply.github.com>
2023-02-06 18:51:54 +0100

    Commit

---
+++ Capitulo_01/Ejercicio05.java
@@ -0,0 +1,15 @@
+package Capitulo_01;
+
+public class Ejercicio05 {
+    public static void main(String args[]){
+        System.out.println("|Lunes|Martes|Miércoles|Jueves|Viernes|");
+        System.out.println("|=====|======|=========|======|=======|");
+        System.out.println("|\033[34mProg\033[39;49m |\033[35mS.I\033[39;49m   |\033[32mE.D\033[39;49m      |\033[34mProg\033[39;49m  |\033[96mFOL\033[39;49m    |");
+        System.out.println("|\033[34mProg\033[39;49m |\033[35mS.I\033[39;49m   |\033[32mE.D\033[39;49m      |\033[34mProg\033[39;49m  |\033[96mFOL\033[39;49m    |");
+        System.out.println("|\033[31mL.M\033[39;49m  |\033[35mS.I\033[39;49m   |\033[34mProg\033[39;49m     |\033[31mL.M\033[39;49m   |\033[96mFOL\033[39;49m    |");
+        System.out.println("|===============RECREO================|");
+        System.out.println("|\033[31mL.M\033[39;49m  |\033[34mProg\033[39;49m  |\033[34mProg\033[39;49m     |\033[31mL.M \033[39;49m  |\033[35mS.I\033[39;49m    |");
+        System.out.println("|\033[33mBBDD\033[39;49m |\033[34mProg\033[39;49m  |\033[33mBBDD \033[39;49m    |\033[33mBBDD\033[39;49m  |\033[35mS.I\033[39;49m    |");
+        System.out.println("|\033[33mBBDD\033[39;49m |\033[32mE.D\033[39;49m   |\033[33mBBDD\033[39;49m     |\033[33mBBDD\033[39;49m  |\033[35mS.I\033[39;49m    |");
+    }
+}
---
+++ Capitulo_01/Ejercicio06.java
@@ -0,0 +1,7 @@
+package Capitulo_01;
+
+public class Ejercicio06 {
+    public static void main(String args[]) {
+        System.out.printf("    *\n***\n  *****\n *******\n*********");
+    }
+}
