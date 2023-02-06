528355d0200ee254dd8e56c1ebc585736efe1f89
AlonsoSMdV <115071431+AlonsoSMdV@users.noreply.github.com>
2023-02-06 18:51:47 +0100

    Commit

---
+++ Capitulo_01/Ejercicio03.java
@@ -0,0 +1,16 @@
+package Capitulo_01;
+
+public class Ejercicio03 {
+    public static void main(String args[]){
+        System.out.printf("%-10s %4s \n","computer","ordenador");
+        System.out.printf("%-10s %4s \n","student","alumno/a");
+        System.out.printf("%-10s %4s \n","cat","gato");
+        System.out.printf("%-10s %4s \n","penguin","pingüino");
+        System.out.printf("%-10s %4s \n","machine","máquina");
+        System.out.printf("%-10s %4s \n","nature","naturaleza");
+        System.out.printf("%-10s %3s \n","light","luz");
+        System.out.printf("%-10s %4s \n","green","verde");
+        System.out.printf("%-10s %4s \n","book","libro");
+        System.out.printf( "%-10s %4s \n","pyramid" ,"pirámide");
+    }
+}
---
+++ Capitulo_01/Ejercicio04.java
@@ -0,0 +1,15 @@
+package Capitulo_01;
+
+public class Ejercicio04 {
+    public static void main(String args[]){
+        System.out.println("|Lunes|Martes|Miércoles|Jueves|Viernes|");
+        System.out.println("|=====|======|=========|======|=======|");
+        System.out.println("|Prog |S.I   |E.D      |Prog  |FOL    |");
+        System.out.println("|Prog |S.I   |E.D      |Prog  |FOL    |");
+        System.out.println("|L.M  |S.I   |Prog     |L.M   |FOL    |");
+        System.out.println("|===============RECREO================|");
+        System.out.println("|L.M  |Prog  |Prog     |L.M   |S.I    |");
+        System.out.println("|BBDD |Prog  |BBDD     |BBDD  |S.I    |");
+        System.out.println("|BBDD |E.D   |BBDD     |BBDD  |S.I    |");
+    }
+}
