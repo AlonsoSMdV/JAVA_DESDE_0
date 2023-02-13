7bd1ef6568ffee5e206b4f80a8004fb865968f94
AlonsoSMdV <115071431+AlonsoSMdV@users.noreply.github.com>
2023-02-13 18:48:32 +0100

    Commit

---
+++ Capitulo_02/Ejercicio01.java
@@ -0,0 +1,16 @@
+package Capitulo_02;
+
+public class Ejercicio01 {
+    public static void main(String args[]){
+        int x = 144;
+        int y = 999;
+        int suma = x + y;
+        int resta = y - x;
+        int multi = x * y;
+        int divi = y / x;
+        System.out.println("Suma = "+ x + " + " + y + " = " + suma);
+        System.out.println("Resta = "+ y + " - " + x + " = " + resta);
+        System.out.println("Multiplicación = "+ x + " * " + y + " = " + multi);
+        System.out.println("División = "+ y + " / " + x + " = " + divi);
+    }
+}
---
+++ Capitulo_02/Ejercicio02.java
@@ -0,0 +1,8 @@
+package Capitulo_02;
+
+public class Ejercicio02 {
+    public static void main(String args[]){
+        String nombre = "Alonso Sánchez Moreno de Vega";
+        System.out.print(nombre);
+    }
+}
