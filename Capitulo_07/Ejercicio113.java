2ed467e5b2af6a2f3ca1d94622a9b2a6f5517b14
Alonso <asanmor011004@gmail.com>
2023-03-21 18:44:33 +0100

    Commit

---
+++ Capitulo_07/Ejercicio113.java
@@ -0,0 +1,54 @@
+package Capitulo_07;
+/* Escribe un programa que rellene un array de 100 elementos con números enteros aleatorios comprendidos entre 0 y 500 (ambos incluidos). A continuación
+el programa mostrará el array y preguntará si el usuario quiere destacar el
+máximo o el mínimo. Seguidamente se volverá a mostrar el array escribiendo
+el número destacado entre dobles asteriscos
+ */
+import java.util.Scanner;
+public class Ejercicio113 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] num = new int[100];
+        int minimo = 500;
+        int maximo = 0;
+        for(int i = 0; i < 100;i++){
+            num[i] = (int)(Math.random()*501);
+            System.out.print(num[i]+" ");
+
+            if (num[i] < minimo) {
+                minimo = num[i];
+            }
+            if (num[i] > maximo) {
+                maximo = num[i];
+            }
+        }
+        System.out.println();
+        System.out.print("¿Qué quieres destacar? (1-mínimo, 2-máximo): ");
+        int destacar = sc.nextInt();
+
+        
+        if(destacar == 1){
+
+            
+            for(int i = 0; i < 100;i++){
+                if(num[i] == minimo){
+                    System.out.print("**"+num[i]+"** ");
+                }else{
+                    System.out.print(num[i]+" ");
+                }
+            }
+        }else if(destacar == 2){
+
+            
+            for(int i = 0; i < 100;i++){
+                if(num[i] == maximo){
+                    System.out.print("**"+num[i]+"** ");
+                }else{
+                    System.out.print(num[i]+" ");
+                }
+    
+            }
+        }
+        sc.close();
+    }
+}
