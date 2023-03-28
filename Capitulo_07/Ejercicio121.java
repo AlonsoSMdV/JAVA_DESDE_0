e056d931c675ea8d7b1ffd479287c22a36c0b9ba
Alonso <asanmor011004@gmail.com>
2023-03-28 12:23:03 +0200

    Commit

---
+++ Capitulo_07/Ejercicio121.java
@@ -0,0 +1,54 @@
+package Capitulo_07;
+/* Escribe un programa que rellene un array de 15 elementos con números enteros
+comprendidos entre 0 y 500 (ambos incluidos). A continuación, se mostrará el
+array “cincuerizado”, según el siguiente criterio: si el número que hay en una
+posición del array es múltiplo de 5, se deja igual, y si no, se cambia por el
+siguiente múltiplo de 5 que exista a partir de él.
+ */
+public class Ejercicio121 {
+    public static void main(String[] args) {
+        int[] arrayPrimero = new int[15];
+        System.out.println("Array original: ");
+        System.out.println("┌────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┐");
+        System.out.printf("│");
+        for(int i = 0;i < 15;i++){
+            System.out.printf("%-8d│",i);
+
+        }
+        System.out.println();
+        System.out.println("├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤");
+
+                System.out.printf("│");
+        for(int i = 0;i < 15;i++){
+            arrayPrimero[i] = (int)(Math.random()*501);
+            System.out.printf("%-8d│",arrayPrimero[i]);
+        }
+        System.out.println();
+        System.out.println("└────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┘");
+
+        for(int i = 0;i< 15;i++){
+            if(arrayPrimero[i] % 5 != 0){
+                do{
+                    arrayPrimero[i]++;
+                }while(arrayPrimero[i] % 5 != 0);
+            }
+        }
+
+        System.out.println("Array resultante: ");
+        System.out.println("┌────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┐");
+        System.out.printf("│");
+        for(int i = 0;i < 15;i++){
+            System.out.printf("%-8d│",i);
+
+        }
+        System.out.println();
+        System.out.println("├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤");
+
+                System.out.printf("│");
+        for(int i = 0;i < 15;i++){
+            System.out.printf("%-8d│",arrayPrimero[i]);
+        }
+        System.out.println();
+        System.out.println("└────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┘");
+    }
+}
