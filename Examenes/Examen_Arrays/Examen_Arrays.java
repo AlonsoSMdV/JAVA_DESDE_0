ee09001f1c9a25cec0fe83aecc0cb954fa280011
Alonso <asanmor011004@gmail.com>
2023-04-25 11:54:23 +0200

    Commit

---
+++ Examenes/Examen_Arrays/Examen_Arrays.java
@@ -0,0 +1,16 @@
+package Examenes.Examen_Arrays;
+
+import java.util.Scanner;
+public class Examen_Arrays {
+    static Scanner sc;
+    protected static void inicializar(){//Iniciar Scanner
+        sc = new Scanner(System.in);
+    }
+
+    protected static void finalizar(){//Fin Scanner    
+        sc.close();
+    }
+    public static void main(String[] args) {
+        
+    }
+}
